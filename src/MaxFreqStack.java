ceeb754beec23dbb4dd204bb6ebbc5c391eed85b
Rahul Kumar Jha <r_kjha@yahoo.com>
2026-09-01 03:59:46 -0700

    Pop maximum frequency element from stack

--- src/MaxFreqStack.java
+++ src/MaxFreqStack.java
@@ -11,6 +11,63 @@ If there is a tie for the most frequent element, the element closest to the stac
 
  */
 
-public class MaxFreqStack {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+class MaxFreqStack {
+
+    Node head;
+    Map<Integer,Integer> freqMap;
+    public MaxFreqStack() {
+        this.head = null;
+        this.freqMap = new HashMap<>();
+    }
+
+    public void push(int val) {
+        Node newNode = new Node(val);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    public int pop() {
+        // keep the frequency of elements into a hashmap
+        Node tempNode = this.head;
+        while(tempNode != null){
+            if(freqMap.containsKey(tempNode.val)){
+                int freq = freqMap.get(tempNode.val);
+                freqMap.put(tempNode.val,freq+1);
+            }else{
+                freqMap.put(tempNode.val, 1);
+            }
+            tempNode = tempNode.next;
+        }
+        // Iterate over the hashmap
+        Set<Integer> keySet = freqMap.keySet();
+        int maxFreq = -1;
+        for(int key:keySet){
+            int val = freqMap.get(key);
+            if(val > maxFreq){
+               maxFreq = val;
+            }
+        }
+
+        return -1;
+    }
 }
+class Node{
+    int val;
+    Node next;
+
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
